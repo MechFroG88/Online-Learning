2d209c5d57ae7baa1c91cab006e8437c25852c40
DavidTan0527 <david_weiucheng@hotmail.com>
2020-03-25 17:02:12 +0800

    Initial setup of client folder; Install skeleton.css and setup localization; Setup login page

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar';
+export default {
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Noto Sans SC', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  .header {
+    position: fixed;
+    top: 0; left: 0;
+    width: 100%;
+    z-index: 999;
+    padding-top: 2rem;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #dfdfdf;
+    section {
+      margin: 0 2rem;
+
+      @media screen and (min-width: 550px) {
+        margin: 0 5rem;
+      }
+    }
+  }
+}
+</style>
---
+++ client/src/views/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="_login">
+    <div class="username-form-group">
+      <label for="username-label">{{ $t('login.fields.username') }}</label>
+      <input class="u-full-width username-input" type="text" id="username-label">
+    </div>
+    <div class="password-form-group">
+      <label for="password-label">{{ $t('login.fields.password') }}</label>
+      <input class="u-full-width password-input" type="password" id="password-label">
+    </div>
+    <div class="submit-button">
+      <input class="button button-primary" type="submit" :value="$t('login.title')">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    
+  })
+}
+</script>
+
+<style lang="scss">
+
+</style>
