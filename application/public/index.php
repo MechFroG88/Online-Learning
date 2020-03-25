b3bf3a9825a8b8a7677eee84d4952cc43225f623
MechFroG88 <kelzzin2@gmail.com>
2020-03-26 02:39:31 +0800

    Fix CORS Issue

--- application/public/index.php
+++ application/public/index.php
@@ -49,6 +49,14 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+if(isset($_SERVER['HTTP_ORIGIN'])){
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Headers: authorization, X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+    header("Access-Control-Max-Age: 1000");
+    header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+}
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
