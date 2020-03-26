acaa7f45ab1d7dddf914f7c4f727886fe45ccd51
MechFroG88 <kelzzin2@gmail.com>
2020-03-26 21:59:55 +0800

    Update Seeder

--- application/database/seeds/UsersTableSeeder.php
+++ application/database/seeds/UsersTableSeeder.php
@@ -16,18 +16,21 @@ class UsersTableSeeder extends Seeder
             'username' => 140145145,
             'type' => 0,
             'cn_name' => '陈科锦',
+            'en_name' => 'TAN KEL ZIN',
             'password' => Hash::make('010801-10-0351'),
         ],
         [
             'username' => 140204204,
             'type' => 0,
             'cn_name' => '陈伟辰',
+            'en_name' => 'TAN WEIU CHENG',
             'password' => Hash::make('010527-14-0579'),
         ],
         [
             'username' => 119999,
             'type' => 0,
             'cn_name' => '教务处',
+            'en_name' => 'JIAO WU CHU',
             'password' => Hash::make('741018-14-5477'),
         ],
         ]);
