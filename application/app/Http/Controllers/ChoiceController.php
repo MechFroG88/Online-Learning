5bd5606b1622c34443856f9ff2253cd93db96f2a
MechFroG88 <kelzzin2@gmail.com>
2020-03-26 23:02:35 +0800

    Update Controller

--- application/app/Http/Controllers/ChoiceController.php
+++ application/app/Http/Controllers/ChoiceController.php
@@ -103,7 +103,7 @@ class ChoiceController extends Controller
             ->update([
                 "method"         => isset($data->method) ? $data->method : "",
                 "link"           => isset($data->link) ? $data->link : "",
-                "streamid"       => isset($data->streamid) ? $data->streamid : "",
+                "streamid"       => isset($data->streamId) ? $data->streamId : "",
                 "streamPassword" => isset($data->streamPassword) ? $data->streamPassword : "",
                 "description"    => isset($data->description) ? $data->description : "",
             ]);
