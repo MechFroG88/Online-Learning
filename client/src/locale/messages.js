f1095dd44fb5b2add59076922901e4f829cc36d8
DavidTan0527 <david_weiucheng@hotmail.com>
2020-03-27 19:45:30 +0800

    Update messages

--- client/src/locale/messages.js
+++ client/src/locale/messages.js
@@ -44,13 +44,24 @@ export default {
       day: 'Period limit per day',
       week: 'Period limit per week',
       start_time: '',
-      end_time: ''
+      end_time: '',
+      start_pick_datetime: 'Start of period for class selection',
+      end_pick_datetime: 'End of period for class selection',
+      username: 'Username',
+      password: 'Password',
+      user_type: 'User type',
+      user_typeSelect: 'Please select user type',
+      passwordHint: 'Leave blank if you wish to not change password'
     },
     timetable: {
       days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
       empty: 'Empty Period',
       select: 'Select',
       modify: 'Modify',
+    },
+    table: {
+      teacher: 'Teacher',
+      admin: 'Admin'
     }
   },
   cn: {
@@ -98,13 +109,24 @@ export default {
       day: '每天选课限制',
       week: '每星期选课限制',
       start_time: '',
-      end_time: ''
+      end_time: '',
+      start_pick_datetime: 'Start of period for class selection',
+      end_pick_datetime: 'End of period for class selection',
+      username: 'Username',
+      password: 'Password',
+      user_type: 'User type',
+      user_typeSelect: 'Please select user type',
+      passwordHint: 'Leave blank if you wish to not change password'
     },
     timetable: {
       days: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
       empty: '小休',
       select: '选择',
       modify: '修改',
+    },
+    table: {
+      teacher: '教师',
+      admin: '管理员'
     }
   },
   bm: {
@@ -152,13 +174,24 @@ export default {
       day: 'Period limit per day',
       week: 'Period limit per week',
       start_time: '',
-      end_time: ''
+      end_time: '',
+      start_pick_datetime: '',
+      end_pick_datetime: '',
+      username: '',
+      password: '',
+      user_type: '',
+      user_typeSelect: '',
+      passwordHint: ''
     },
     timetable: {
       days: ['Ahad', 'Isnin', 'Selasa', 'Rabu', 'Khamis', 'Jumaat', 'Sabtu'],
       empty: 'Waktu Rehat',
       select: 'Pilih',
       modify: 'Ubah informasi',
+    },
+    table: {
+      teacher: '',
+      admin: ''
     }
   }
 }
